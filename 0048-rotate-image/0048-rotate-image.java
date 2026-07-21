6b1be0b39eddfac2fa865c246e6b4874452b3ac8
Rishikesh Palkar <rishikeshpalkar87@gmail.com>
2026-07-21 18:04:04 +0530

    Time: 0 ms (100%), Space: 43.5 MB (86.5%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        // transpose of matrix
+        int m = matrix.length;
+       
+            for(int i = 0 ; i<m ; i++){
+                for(int j = i+1 ; j<m ; j++){
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp ;
+                }
+            }
+
+            //reflection
+            for(int i=0 ; i<m ; i++){
+                for(int j=0 ; j<m/2 ; j++){
+                     int temp = matrix[i][j];
+                    matrix[i][j] = matrix[i][m-j-1];
+                    matrix[i][m-j-1] = temp ;
+                }
+            }
+          // print
+          
+    }
+}
