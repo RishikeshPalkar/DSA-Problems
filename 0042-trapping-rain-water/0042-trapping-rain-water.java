b6a172830cddd90e1dc77232038e6407a5a1f567
Rishikesh Palkar <rishikeshpalkar87@gmail.com>
2026-07-26 17:22:46 +0530

    Time: 1 ms (59.78%), Space: 48.6 MB (22.77%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,23 +1,23 @@
 class Solution {
     public int trap(int[] height) {
-        int l_wall =0 ;
-        int r_wall = 0 ;
+        int l_wall = 0;
+        int r_wall = 0;
         int n = height.length;
-        int max_left[] =new int[n];
-        int max_right[] = new int[n];
+        int left_max[] = new int [n];
+        int right_max[] = new int[n];
 
-        for(int i  = 0, j = n-1 ; i<height.length ; i++, j--){
-            max_left[i] = l_wall;
+        for(int i = 0 , j=n-1 ; i<n; i++,j--){
+            left_max[i] = l_wall;
             l_wall = Math.max(l_wall , height[i]);
 
-            max_right[j] = r_wall;
-            r_wall = Math.max(r_wall, height[j]);
-        } 
-        int sum = 0;
-        for(int i = 0 ; i<n ; i++){
-            int pot = Math.min(max_left[i] , max_right[i]);
-            sum += Math.max(0 , pot - height[i]);
+            right_max[j] = r_wall;
+            r_wall = Math.max(r_wall , height[j]);
         }
-        return sum ;
+        int count = 0;
+        for(int i = 0 ; i<n; i++){
+            int potential = Math.min(left_max[i] , right_max[i]);
+           count += Math.max(0, potential - height[i]);
+        }
+        return count;
     }
 }
