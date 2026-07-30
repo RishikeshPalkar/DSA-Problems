0dc8ff504fcefa4c925e15d9218a9444760a8cff
Rishikesh Palkar <rishikeshpalkar87@gmail.com>
2026-07-30 22:26:54 +0530

    Time: 0 ms (100%), Space: 46.4 MB (94.18%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,19 +11,14 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while(fast != null && fast.next != null){
-            fast = fast.next.next;
-            slow = slow.next;
-
-            if(fast == slow){
-                return true;
-            }
-
-        }
-
-        return false;
+       ListNode fast = head;
+       ListNode slow = head;
+       while(fast != null && fast.next != null){
+        fast = fast.next.next;
+        slow = slow.next;
+        if(slow ==fast) return true;
+        
+       }
+       return false;
     }
 }
