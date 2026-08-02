329ac76ce68f653d5e8082dd13059504b080b075
Rishikesh Palkar <rishikeshpalkar87@gmail.com>
2026-08-02 12:46:19 +0530

    Time: 0 ms (100%), Space: 42.8 MB (84.77%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,21 +1,17 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int length = 0;
-        boolean counting = false;
-
-        for (char c : s.toCharArray()) {
-            if (c != ' ') {
-                if (!counting) {
-                    counting = true;
-                    length = 1;
-                } else {
-                    length++;
-                }
-            } else {
-                counting = false;
-            }
+       int length = 0;
+        int i = s.length() - 1;
+        
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
         }
-
-        return length;        
+        
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+        
+        return length;
     }
 }
